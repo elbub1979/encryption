33a01edf4da67f05856040d1352aa93d706787ec
elbub1979 <ilya.leuh09@gmail.com>
2022-04-05 22:14:14 +0300

    added sha2

--- main.rb
+++ main.rb
@@ -7,16 +7,18 @@ puts 'Введите слово или фразу для шифрования:'
 encrypt = gets.chomp
 
 puts 'Каким способом зашифровать:'
-puts <<~END
-  1. MD5
-  2. SHA1
-END
+puts <<~ENCRYPTION
+1. MD5
+2. SHA1
+3. SHA2
+ENCRYPTION
 choice = 0
 
-choice = Integer(gets, exception: false) until (1..2).include?(choice)
+choice = Integer(gets, exception: false) until (1..3).include?(choice)
 result = case choice
          when 1 then Digest::MD5.hexdigest encrypt
          when 2 then Digest::SHA1.hexdigest encrypt
+         when 3 then Digest::SHA2.hexdigest encrypt
          end
 
 puts 'Вот что получилось:'
